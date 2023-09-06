019f012e5ce8854e64a62e6bb8b968399db97e0e
suryaruddaraju <surya.ruddaraju@gmail.com>
2023-09-06 15:14:19 -0700

    fix price calc bug

--- client3.py
+++ client3.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price/ask_price)/2
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
@@ -60,4 +60,4 @@ if __name__ == "__main__":
             stkPrices[stock] = price # set the price of the stock in our array to calculate ratio later
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(stkPrices["ABC"], stkPrices["DEF"])) #calculate price ratio between stocks ABC and DEF
+        print("Ratio %s" % getRatio(stkPrices["ABC"], stkPrices["DEF"])) #calculate price ratio between stocks ABC and
